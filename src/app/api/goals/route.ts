9661f0f952cafce10d0af0f9d960a486f71e1e5c
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2025-08-07 23:58:47 +0800

    feat(api): add GET endpoint to get goals

--- src/app/api/goals/route.ts
+++ src/app/api/goals/route.ts
@@ -1,9 +1,36 @@
 import { GoalData } from "@/app/_types/type";
-import { getLoggedInUser } from "@/utils/auth";
+import { getLoggedInUser, verifyAuthToken } from "@/utils/auth";
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
+
+// GET: /goals ユーザー_目標一覧取得
+export const GET = async (request: NextRequest) => {
+  try {
+    const user = await verifyAuthToken(request);
+    const goals = await prisma.goal.findMany({
+      where: {
+        userId: user.id,
+      },
+    });
+
+    if (!goals) {
+      return NextResponse.json(
+        { status: "目標を取得できませんでした" },
+        { status: 400 },
+      );
+    }
+
+    return NextResponse.json<{ status: string; goal: GoalData[] }>(
+      { status: "OK", goal: goals },
+      { status: 200 },
+    );
+  } catch (error) {
+    if (error instanceof Error)
+      return NextResponse.json({ status: error.message }, { status: 400 });
+  }
+};
 
 // POST: /goals ユーザー_目標新規作成
 

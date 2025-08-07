ef824acf8b829c2f41c28454e66d80998d6d7abb
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2025-08-07 22:50:56 +0800

    feat(api): add GET, PATCH endpoint to create me(profile)

--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -5,8 +5,34 @@ import { ProfileData } from "@/app/_types/type";
 
 const prisma = new PrismaClient();
 
-// POST: /me ユーザー_プロフィール新規作成
+// GET: /me ユーザー_プロフィール取得
+export const GET = async (request: NextRequest) => {
+  try {
+    const user = await verifyAuthToken(request);
+    const profile = await prisma.user.findUnique({
+      where: {
+        id: user.id,
+      },
+    });
 
+    if (!profile) {
+      return NextResponse.json(
+        { status: "プロフィール情報が取得できませんでした" },
+        { status: 400 },
+      );
+    }
+
+    return NextResponse.json<{ status: string; user: ProfileData }>(
+      { status: "OK", user: profile },
+      { status: 200 },
+    );
+  } catch (error) {
+    if (error instanceof Error)
+      return NextResponse.json({ status: error.message }, { status: 400 });
+  }
+};
+
+// POST: /me ユーザー_プロフィール新規作成
 type CreateProfileRequestBody = {
   id: string; // Supabaseのuser.id
   email: string; // Supabaseのuser.email
@@ -33,6 +59,37 @@ export const POST = async (request: NextRequest) => {
       status: "OK",
       user: profile,
     });
+  } catch (error) {
+    if (error instanceof Error)
+      return NextResponse.json({ status: error.message }, { status: 400 });
+  }
+};
+
+// PATCH: /me ユーザー_プロフィール更新
+type UpdateProfileRequestBody = {
+  name: string;
+  colorTheme: "ORIGINAL" | "COOL" | "WARM" | "NATURE" | "SUNSHINE";
+};
+
+export const PATCH = async (request: NextRequest) => {
+  try {
+    const user = await verifyAuthToken(request);
+
+    const { name, colorTheme }: UpdateProfileRequestBody = await request.json();
+    const profile = await prisma.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        name,
+        colorTheme,
+      },
+    });
+
+    return NextResponse.json<{ status: string; user: ProfileData }>(
+      { status: "OK", user: profile },
+      { status: 200 },
+    );
   } catch (error) {
     if (error instanceof Error)
       return NextResponse.json({ status: error.message }, { status: 400 });

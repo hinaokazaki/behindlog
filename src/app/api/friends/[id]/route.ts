0464128086481414e38fc87b28b1c24482f9de77
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2025-09-14 01:26:40 +0800

    feat(API): add DELETE endpoint to delete friendship

--- src/app/api/friends/[id]/route.ts
+++ src/app/api/friends/[id]/route.ts
@@ -3,7 +3,6 @@ import { prisma } from "@/lib/prisma";
 import { getLoggedInUser } from "@/utils/auth";
 
 // PATCH: /api/friends/:id ユーザー_友達申請承認
-
 export const PATCH = async (
   request: NextRequest,
   { params }: { params: { id: string } },
@@ -20,15 +19,15 @@ export const PATCH = async (
     }
 
     // userId2確認
-    const friendshipForUserId2Check = await prisma.friendship.findUnique({
+    const checkFriendshipForUserId2 = await prisma.friendship.findUnique({
       where: {
         id: Number(id),
       },
     });
 
     if (
-      !friendshipForUserId2Check ||
-      friendshipForUserId2Check.userId2 !== user.id
+      !checkFriendshipForUserId2 ||
+      checkFriendshipForUserId2.userId2 !== user.id
     ) {
       return NextResponse.json({ error: "Unauthorized" }, { status: 403 });
     }
@@ -40,6 +39,41 @@ export const PATCH = async (
       data: {
         status: status,
         respondedAt: new Date(),
+      },
+    });
+
+    return NextResponse.json({ status: "OK", friendship }, { status: 200 });
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ status: error.message }, { status: 400 });
+    }
+  }
+};
+
+// DELETE: /api/friends/:id ユーザー_友達解除
+export const DELETE = async (
+  request: NextRequest,
+  { params }: { params: { id: string } },
+) => {
+  const { id } = params;
+  try {
+    const user = await getLoggedInUser(request);
+
+    const checkFriendshipForDelete = await prisma.friendship.findUnique({
+      where: { id: Number(id) },
+    });
+
+    if (
+      !checkFriendshipForDelete ||
+      (checkFriendshipForDelete.userId1 !== user.id &&
+        checkFriendshipForDelete.userId2 !== user.id)
+    ) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 403 });
+    }
+
+    const friendship = await prisma.friendship.delete({
+      where: {
+        id: Number(id),
       },
     });
 

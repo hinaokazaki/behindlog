6f8fb8f89f3b3cb7985fed73df7234c55df55c15
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2025-09-18 11:25:03 +0800

    feat(test): make function to get access token

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -6,8 +6,18 @@ import Input from "../_components/Input";
 import Label from "../_components/Label";
 import Button from "../_components/Button";
 import Textarea from "../_components/Textarea";
+import { useEffect } from "react";
+import { supabase } from "@/utils/supabase";
 
 export default function InputTestPage() {
+  useEffect(() => {
+    const getToken = async () => {
+      const { data } = await supabase.auth.getSession();
+      console.log("Access Token:", data.session?.access_token);
+    };
+    getToken();
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-white p-8">
       <div className="w-full max-w-sm space-y-6">

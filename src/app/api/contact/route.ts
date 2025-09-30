1c44032e438e6236e65dd3ef476f06ae822b0967
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2025-10-01 01:03:25 +0800

    fix(api/contact): update zod schema and add typed responses

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,12 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import {
+  Contact,
   ContactResponse,
-  contactResponseSchema,
+  contactSchema,
   CreateContactRequest,
   createContactRequestSchema,
 } from "@/schemas/contact";
 import { withUserTimezone } from "@/lib/timezone";
+import { ErrorResponse } from "@/schemas/common";
 
 // POST: /contact ユーザー_お問い合わせ作成
 
@@ -20,13 +22,19 @@ export const POST = async (request: NextRequest) => {
       data: body,
     });
 
-    const safeContact: ContactResponse = contactResponseSchema.parse(
+    const safeContact: Contact = contactSchema.parse(
       withUserTimezone(contact, ["createdAt"], "UTC"),
     );
 
-    return NextResponse.json({ contact: safeContact }, { status: 200 });
+    return NextResponse.json<ContactResponse>(
+      { status: "OK" },
+      { status: 200 },
+    );
   } catch (error) {
     if (error instanceof Error)
-      return NextResponse.json({ status: error.message }, { status: 400 });
+      return NextResponse.json<ErrorResponse>(
+        { error: error.message },
+        { status: 400 },
+      );
   }
 };
--- src/schemas/contact.ts
+++ src/schemas/contact.ts
@@ -1,5 +1,15 @@
 import { z } from "zod";
 
+// 共通のcontactモデル
+export const contactSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  email: z.string().email(),
+  message: z.string(),
+  createdAt: z.string(),
+  updatedAt: z.string(),
+});
+
 // リクエスト用
 export const createContactRequestSchema = z.object({
   name: z.string(),
@@ -13,5 +23,6 @@ export const contactResponseSchema = z.object({
 });
 
 // 型を生成
+export type Contact = z.infer<typeof contactSchema>;
 export type CreateContactRequest = z.infer<typeof createContactRequestSchema>;
 export type ContactResponse = z.infer<typeof contactResponseSchema>;

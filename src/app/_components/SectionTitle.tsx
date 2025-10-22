8d03d70bb87c8a5c3424e82f6ba852bcc33f83e7
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2025-10-22 11:50:22 +0800

    feat: add public style option to SectionTitle component

--- src/app/_components/SectionTitle.tsx
+++ src/app/_components/SectionTitle.tsx
@@ -3,14 +3,18 @@ import React from "react";
 
 type Props = {
   title: string;
+  isPublic?: boolean;
 };
 
-const SectionTitle: React.FC<Props> = ({ title }) => {
+const SectionTitle: React.FC<Props> = ({ title, isPublic = false }) => {
+  const textPlace = isPublic ? "text-center" : "";
+  const lineWidth = isPublic ? "w-[70%]" : "";
+
   return (
-    <div className="mb-14 text-center">
+    <div className={`mb-14 ${textPlace}`}>
       <p className="inline-block font-en text-heading-1 font-bold text-primary">
         {title}
-        <span className="mx-auto mt-4 block h-1 w-[70%] bg-red-300" />
+        <span className={`mx-auto mt-4 block h-1 ${lineWidth} bg-red-300`} />
       </p>
     </div>
   );

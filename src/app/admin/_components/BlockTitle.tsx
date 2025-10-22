84fad05c82cca76282da8f790a737e2ad2b93242
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2025-10-22 13:03:16 +0800

    feat: create BlockTitle component

---
+++ src/app/admin/_components/BlockTitle.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React from "react";
+
+type Props = {
+  title: string;
+  mode?: "section" | "modal";
+};
+
+const BlockTitle: React.FC<Props> = ({ title, mode = "section" }) => {
+  const styles = {
+    section: "text-primary text-heading-2 font-en",
+    modal: "text-base text-heading-3 font-jp",
+  };
+
+  return <p className={`mb-7 font-bold ${styles[mode]}`}>{title}</p>;
+};
+
+export default BlockTitle;

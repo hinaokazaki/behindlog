325f2e28b318c3e22f5911f8530fd1046fd82543
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2025-12-28 00:15:32 +0800

    fix: adjust UI styles

--- src/app/_components/SectionTitle.tsx
+++ src/app/_components/SectionTitle.tsx
@@ -7,11 +7,11 @@ type Props = {
 };
 
 const SectionTitle: React.FC<Props> = ({ title, isPublic = false }) => {
-  const textPlace = isPublic ? "text-center" : "";
+  const textPlace = isPublic ? "text-center mb-14" : "mb-7";
   const lineWidth = isPublic ? "w-[70%]" : "";
 
   return (
-    <div className={`mb-14 ${textPlace}`}>
+    <div className={textPlace}>
       <p className="inline-block font-en text-heading-1 font-bold text-primary">
         {title}
         <span className={`mx-auto mt-4 block h-1 ${lineWidth} bg-red-300`} />

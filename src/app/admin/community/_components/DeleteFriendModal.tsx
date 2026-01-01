f60061806bedbdcca61d9497214eeae26771cb42
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-01-02 02:18:06 +0800

    feat: create DeleteFriendModal component

---
+++ src/app/admin/community/_components/DeleteFriendModal.tsx
@@ -0,0 +1,69 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+import { Modal } from "../../_components/Modal";
+import { FriendList } from "@/schemas/friend";
+import Button from "@/app/_components/Button";
+
+interface Props {
+  isOpen: boolean;
+  friend: FriendList | null;
+  onClose: () => void;
+  onConfirm: () => void;
+  isSubmitting?: boolean;
+}
+
+const DeleteFriendModal: React.FC<Props> = ({
+  isOpen,
+  friend,
+  onClose,
+  onConfirm,
+  isSubmitting,
+}) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      {friend && (
+        <div>
+          <div>
+            <h1 className="mb-6 text-base text-subtitle-top font-bold">
+              友達の削除
+            </h1>
+            <p className="mb-2 flex items-center text-base text-body">
+              <Image
+                src="/profile-red.png"
+                width={30}
+                height={30}
+                alt="プロフィール"
+                className="mx-2"
+              />
+              <span className="mr-2 text-base font-bold">
+                {friend.friend.name}
+              </span>
+              を友達リストから削除しますか？
+            </p>
+          </div>
+          <div className="mt-8 flex items-center justify-evenly">
+            <Button
+              type="button"
+              disabled={isSubmitting}
+              onClick={onClose}
+              color="red"
+              variant="outlined"
+              label="キャンセル"
+            />
+            <Button
+              type="button"
+              disabled={isSubmitting}
+              onClick={onConfirm}
+              color="red"
+              variant="filled"
+              label="友達リストから削除"
+            />
+          </div>
+        </div>
+      )}
+    </Modal>
+  );
+};
+
+export default DeleteFriendModal;

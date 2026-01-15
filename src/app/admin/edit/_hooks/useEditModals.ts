7771dc57a323be4f7d1a4c10feb5c29f74df8b51
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-01-15 08:44:47 +0800

    feat: add useEditModals hook to manage EditModal state

---
+++ src/app/admin/edit/_hooks/useEditModals.ts
@@ -0,0 +1,53 @@
+import { Goal } from "@/schemas/goal";
+import { useState } from "react";
+
+export const useEditModals = () => {
+  // 目標新規作成モーダル
+  const [isCreateGoalOpen, setIsCreateGoalOpen] = useState(false);
+  const openCreateGoal = () => setIsCreateGoalOpen(true);
+  const closeCreateGoal = () => setIsCreateGoalOpen(false);
+
+  // 目標編集モーダル
+  const [selectedGoal, setSelectedGoal] = useState<Goal | null>(null);
+  const openUpdateGoal = (goal: Goal) => setSelectedGoal(goal);
+  const closeUpdateGoal = () => setSelectedGoal(null);
+
+  // 目標削除モーダル
+  // const [selectedGoal, setSelectedGoal] = useState<Goal | null>(null);
+  const openDeleteGoal = (goal: Goal) => setSelectedGoal(goal);
+  const closeDeleteGoal = () => setSelectedGoal(null);
+
+  // Todo新規作成モーダル
+  const [isCreateTodoOpen, setIsCreateTodoOpen] = useState(false);
+  const openCreateTodo = () => setIsCreateTodoOpen(true);
+  const closeCreateTodo = () => setIsCreateTodoOpen(false);
+
+  // Todo編集モーダル
+
+  // Todo削除モーダル
+
+  return {
+    // 目標新規作成モーダル
+    isCreateGoalOpen,
+    openCreateGoal,
+    closeCreateGoal,
+
+    // 目標編集モーダル
+    selectedGoal,
+    openUpdateGoal,
+    closeUpdateGoal,
+
+    // 目標削除モーダル
+    openDeleteGoal,
+    closeDeleteGoal,
+
+    // Todo新規作成モーダル
+    isCreateTodoOpen,
+    openCreateTodo,
+    closeCreateTodo,
+
+    // Todo編集モーダル
+
+    // Todo削除モーダル
+  };
+};

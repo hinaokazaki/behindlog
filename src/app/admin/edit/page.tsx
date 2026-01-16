73ae8aef9be03f2f4971eafeaff7c3dd110346d6
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-01-16 13:04:06 +0800

    feat: add GoalSection component

---
+++ src/app/admin/edit/page.tsx
@@ -0,0 +1,11 @@
+import SectionTitle from "@/app/_components/SectionTitle";
+import GoalSection from "./_components/GoalSection";
+
+export default function EditPage() {
+  return (
+    <div>
+      <SectionTitle title="Edit your record - 編集" />
+      <GoalSection/>
+    </div>
+  );
+}

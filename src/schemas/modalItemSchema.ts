2954e54b56a02a3728841e3ffce352094900eff8
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-01-17 23:13:34 +0800

    refactor: temporarily disable dateRange schema and add deadline schema

--- src/schemas/modalItemSchema.ts
+++ src/schemas/modalItemSchema.ts
@@ -2,14 +2,15 @@ import { z } from "zod";
 
 export const modalItemSchema = z.object({
   title: z.string().min(1, "タイトルは必須です"),
-  dataRange: z
-    .object({
-      from: z.date().nullable(),
-      to: z.date().nullable(),
-    })
-    .refine((v) => !v.from || !v.to || v.from <= v.to, {
-      message: "開始日は終了日より前にしてください",
-    }),
+  deadline: z.string().min(1)
+  // dataRange: z
+  //   .object({
+  //     from: z.date().nullable(),
+  //     to: z.date().nullable(),
+  //   })
+  //   .refine((v) => !v.from || !v.to || v.from <= v.to, {
+  //     message: "開始日は終了日より前にしてください",
+  //   }),
 });
 
 export type modalItemForm = z.infer<typeof modalItemSchema>;

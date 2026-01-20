052e3063bb9b5ca69ce5f0daf8b74bc810718c15
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-01-21 00:45:39 +0800

    feat: add TodoSection component

--- src/app/admin/edit/page.tsx
+++ src/app/admin/edit/page.tsx
@@ -1,11 +1,13 @@
 import SectionTitle from "@/app/_components/SectionTitle";
 import GoalSection from "./_components/GoalSection";
+import TodoSection from "./_components/TodoSection";
 
 export default function EditPage() {
   return (
     <div>
       <SectionTitle title="Edit your record - 編集" />
-      <GoalSection/>
+      <GoalSection />
+      <TodoSection />
     </div>
   );
 }

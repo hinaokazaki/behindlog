73b73b50aad1b92cadbb0ec5fda3ee7d62c29648
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-01-22 13:09:45 +0800

    feat: add todoStatusOptions for Satus select field of todoEditForm

--- src/constants/themeOptions.ts
+++ src/constants/themeOptions.ts
@@ -5,3 +5,8 @@ export const themeOptions = [
   { value: "NATURE", label: "ネイチャー" },
   { value: "SUNSHINE", label: "サンシャイン" },
 ];
+
+export const todoStatusOptions = [
+  { value: "false", label: "未完了" },
+  { value: "true", label: "完了" },
+];

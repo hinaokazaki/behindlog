22ee6846ed6df4e9e2df1908de6838a6e773b332
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-01-22 13:10:06 +0800

    feat: add osCompleted Schema

--- src/schemas/todoModalSchema.ts
+++ src/schemas/todoModalSchema.ts
@@ -2,6 +2,9 @@ import { z } from "zod";
 
 export const todoModalSchema = z.object({
   title: z.string().min(1, "ToDoを入力してください"),
+  isCompleted: z.enum(["true", "false"], {
+    required_error: "ステータスを選択してください",
+  }),
   dueDate: z.string().min(1, "期限を選択してください"),
 });
 

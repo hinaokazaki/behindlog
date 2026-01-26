d531345152dd7d2702d77ece5e9e13cbe84006e1
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-01-26 23:49:15 +0800

    feat: move todoStatusOptions from themeOptions

--- src/constants/themeOptions.ts
+++ src/constants/themeOptions.ts
@@ -5,8 +5,3 @@ export const themeOptions = [
   { value: "NATURE", label: "ネイチャー" },
   { value: "SUNSHINE", label: "サンシャイン" },
 ];
-
-export const todoStatusOptions = [
-  { value: "false", label: "未完了" },
-  { value: "true", label: "完了" },
-];

888232736b42a7cf54abef7de6fb22e282a6dc2b
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-01-31 23:35:25 +0800

    feat: add CommitTimeSection

--- src/app/admin/edit/page.tsx
+++ src/app/admin/edit/page.tsx
@@ -1,6 +1,7 @@
 import SectionTitle from "@/app/_components/SectionTitle";
 import GoalSection from "./_components/GoalSection";
 import TodoSection from "./_components/TodoSection";
+import CommitTimeSection from "./_components/CommitTimeSection";
 
 export default function EditPage() {
   return (
@@ -8,6 +9,7 @@ export default function EditPage() {
       <SectionTitle title="Edit your record - 編集" />
       <GoalSection />
       <TodoSection />
+      <CommitTimeSection />
     </div>
   );
 }

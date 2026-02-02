7a5835a4bb28d56781035e57e6a9b0462bca0c6b
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-02-02 22:27:34 +0800

    refactor: remove unnecessary div

--- src/app/admin/_components/AddNewButton.tsx
+++ src/app/admin/_components/AddNewButton.tsx
@@ -8,24 +8,22 @@ interface Props {
 
 const AddNewButton: React.FC<Props> = ({ handleAdding, label }) => {
   return (
-    <div className="mt-4">
-      <button
-        type="button"
-        onClick={handleAdding}
-        className={`flex items-center gap-2 rounded-lg p-3`}
-      >
-        <div className="flex">
-          <Image
-            src="/add.png"
-            width={25}
-            height={25}
-            alt="追加"
-            className="mx-2"
-          />
-          <span className="mr-2 text-base font-bold">新しい{label}</span>
-        </div>
-      </button>
-    </div>
+    <button
+      type="button"
+      onClick={handleAdding}
+      className={`mt-2 flex items-center gap-2 rounded-lg p-3`}
+    >
+      <div className="flex">
+        <Image
+          src="/add.png"
+          width={25}
+          height={25}
+          alt="追加"
+          className="mx-2"
+        />
+        <span className="mr-2 text-base font-bold">新しい{label}</span>
+      </div>
+    </button>
   );
 };
 

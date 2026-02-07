04f12fbb310c9dc0602adbc9a82606d985760cf3
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-02-07 23:13:13 +0800

    feat: add totalStudyTimeByPeriod logic and update UI layout

--- src/app/admin/records/[date]/page.tsx
+++ src/app/admin/records/[date]/page.tsx
@@ -174,12 +174,17 @@ export default function RecordsPage({ params }: { params: { date: string } }) {
         payload,
       );
       router.refresh();
+      committimeSummaryQuery.mutate();
     } catch (error) {
       console.error("save records failed", error);
     } finally {
       setIsSubmitting(false);
     }
   };
+
+  // totalStudyTimeByPeriod
+  const totalStudyTimeByPeriod =
+    committimeSummaryQuery.data?.totalStudyTime.totalStudyTimeByPeriod ?? 0;
 
   return (
     <div>
@@ -203,51 +208,80 @@ export default function RecordsPage({ params }: { params: { date: string } }) {
       </section>
       <section className="mx-auto mb-4 w-full min-w-[580px] max-w-[760px] rounded-3xl bg-white p-6 shadow-md">
         <BlockTitle title="Commit time" />
-        <div>
-          <p className="text-base">目標学習時間</p>
-          <p className="text-base">
-            {page?.committime.targetTime ? page.committime.targetTime / 60 : 0}
-            時間
-          </p>
-          <span className="text-base">
-            [{`${page?.committime.startDate} - ${page?.committime.endDate}`}]
-          </span>
-        </div>
-        <hr />
-        <div>
+        <div className="mt-2 flex items-end justify-between">
           <div>
-            <p className="text-base">今日の学習時間</p>
-            <div>
-              <Input
-                type="number"
-                name="studyHours"
-                value={studyTime.studyHours}
-                onChange={(e) =>
-                  setStudyTime((p) => ({
-                    ...p,
-                    studyHours: Number(e.target.value),
-                  }))
-                }
-              />
-              時間
-              <Input
-                type="number"
-                name="studyMinutes"
-                min={0}
-                max={59}
-                value={studyTime.studyMinutes}
-                onChange={(e) =>
-                  setStudyTime((p) => ({
-                    ...p,
-                    studyMinutes: Number(e.target.value),
-                  }))
-                }
-              />
-              分
+            <p className="text-base text-form-text">目標学習時間</p>
+            <p className="ml-4 mt-4 text-base font-bold">
+              {page?.committime.targetTime
+                ? Math.floor(page.committime.targetTime / 60)
+                : 0}
+              <span className="ml-8 text-base text-form-text">時間</span>
+            </p>
+          </div>
+
+          <p className="text-base text-sm font-semibold">
+            [{page?.committime.startDate ?? "----"} -{" "}
+            {page?.committime.endDate ?? "----"}]
+          </p>
+        </div>
+
+        <div className="mt-4 border-t border-gray-200" />
+
+        <div className="mt-4 grid grid-cols-2 gap-8">
+          <div>
+            <p className="text-base text-form-text">今日の学習時間</p>
+
+            <div className="mt-4 flex items-center gap-3">
+              <div className="flex items-center gap-2">
+                <Input
+                  type="number"
+                  name="studyHours"
+                  value={studyTime.studyHours}
+                  className="w-20 rounded-md border-2 p-2 text-center"
+                  onChange={(e) =>
+                    setStudyTime((p) => ({
+                      ...p,
+                      studyHours: Number(e.target.value),
+                    }))
+                  }
+                />
+                <span className="text-base text-form-text font-medium">
+                  時間
+                </span>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <Input
+                  type="number"
+                  name="studyMinutes"
+                  min={0}
+                  max={59}
+                  value={studyTime.studyMinutes}
+                  className="w-20 rounded-md border-2 p-2 text-center"
+                  onChange={(e) =>
+                    setStudyTime((p) => ({
+                      ...p,
+                      studyMinutes: Number(e.target.value),
+                    }))
+                  }
+                />
+                <span className="text-base text-form-text font-medium">分</span>
+              </div>
             </div>
           </div>
+
           <div>
-            <p className="text-base">合計学習時間</p>
+            <p className="text-base text-form-text">合計学習時間</p>
+            <p className="mt-4 text-base font-semibold">
+              {Math.floor(totalStudyTimeByPeriod / 60)}
+              <span className="mx-4 text-base text-form-text font-medium">
+                時間
+              </span>
+              {totalStudyTimeByPeriod % 60}
+              <span className="mx-4 text-base text-form-text font-medium">
+                分
+              </span>
+            </p>
           </div>
         </div>
       </section>

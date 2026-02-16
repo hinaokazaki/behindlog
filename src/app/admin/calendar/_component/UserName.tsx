f0715ab5d605e9818d1af144941c33ae8d89f5c5
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-02-16 23:31:08 +0800

    feat: add key and css style to UserName UI component

--- src/app/admin/calendar/_component/UserName.tsx
+++ src/app/admin/calendar/_component/UserName.tsx
@@ -3,22 +3,27 @@ import Image from "next/image";
 import Link from "next/link";
 
 type Props = {
+  key?: string;
   name: string;
   link: string;
 };
 
-const UserName: React.FC<Props> = ({ name, link }) => {
+const UserName: React.FC<Props> = ({ key, name, link }) => {
   return (
-    <Link href={link}>
+    <Link
+      href={link}
+      key={key}
+      className="flex cursor-pointer items-center border-secondary hover:rounded-lg hover:border-[2px]"
+    >
       <div className="flex">
         <Image
           src="/profile-red.png"
-          width={30}
-          height={30}
+          width={20}
+          height={20}
           alt="プロフィール"
           className="mx-2"
         />
-        <span className="mr-2 text-base font-bold">{name}</span>
+        <span className="font-body mr-2 text-base font-[12px]">{name}</span>
       </div>
     </Link>
   );

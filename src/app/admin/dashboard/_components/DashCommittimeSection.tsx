aefeffc470021c00fae771a862963be49e503777
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-03-19 22:04:57 +0800

    feat: add DashCommittimeSection component

--- src/app/admin/dashboard/_components/DashCommittimeSection.tsx
+++ src/app/admin/dashboard/_components/DashCommittimeSection.tsx
@@ -3,11 +3,15 @@
 import Loading from "@/app/_components/Loading";
 import { useCommittimeQuery } from "../../_hooks/useCommittimeQuery";
 import BlockTitle from "../../_components/BlockTitle";
+import { useCommittimeSummaryQuery } from "../../_hooks/useCommittimeSummaryQuery";
 
 const DashCommittimeSection: React.FC = () => {
   const committimeQuery = useCommittimeQuery();
+  const committimeSummaryQuery = useCommittimeSummaryQuery();
 
-  if (committimeQuery.isLoading) return <Loading />;
+  if (committimeQuery.isLoading || committimeSummaryQuery.isLoading)
+    return <Loading />;
+
   if (committimeQuery.error)
     return (
       <p>
@@ -15,11 +19,38 @@ const DashCommittimeSection: React.FC = () => {
       </p>
     );
 
-  const committime = committimeQuery.data?.committime;
+  if (committimeQuery.error)
+    return (
+      <p>
+        合計時間の取得でエラーが発生しました:{" "}
+        {committimeSummaryQuery.error.message}
+      </p>
+    );
+
+  const committime = committimeQuery.data
+    ? committimeQuery.data.committime
+    : null;
+  const committimeSummary = committimeSummaryQuery.data
+    ? committimeSummaryQuery.data.totalStudyTime
+    : null;
 
   return (
     <section className="mx-auto mb-4 w-full min-w-[580px] max-w-[760px] rounded-3xl bg-white p-6 shadow-md">
       <BlockTitle title="Commit Time" />
+      <div className="mt-2 flex items-end justify-between">
+        <div>
+          <p className="text-base text-form-text">目標学習時間</p>
+          <p className="ml-4 mt-4 text-base font-bold">
+            {committime.targetTime ? Math.floor(committime.targetTime / 60) : 0}
+            <span className="ml-8 text-base text-form-text">時間</span>
+          </p>
+        </div>
+
+        <p className="text-base text-sm font-semibold">
+          [{displayCommittime.startDate ?? "----"} -{" "}
+          {displayCommittime.endDate ?? "----"}]
+        </p>
+      </div>
     </section>
   );
 };

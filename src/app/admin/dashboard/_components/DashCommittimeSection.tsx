10c173efd4eb535b0110e71f0fa631a4fa8bfffa
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-03-20 22:00:21 +0800

    feat: add totalSutudyTimeByPeriod section

--- src/app/admin/dashboard/_components/DashCommittimeSection.tsx
+++ src/app/admin/dashboard/_components/DashCommittimeSection.tsx
@@ -30,9 +30,13 @@ const DashCommittimeSection: React.FC = () => {
   const committime = committimeQuery.data
     ? committimeQuery.data.committime
     : null;
+
   const committimeSummary = committimeSummaryQuery.data
     ? committimeSummaryQuery.data.totalStudyTime
     : null;
+
+  const totalStudyTimeByPeriod =
+    committimeSummaryQuery.data?.totalStudyTime.totalStudyTimeByPeriod ?? 0;
 
   return (
     <section className="mx-auto mb-4 w-full min-w-[580px] max-w-[760px] rounded-3xl bg-white p-6 shadow-md">
@@ -41,14 +45,26 @@ const DashCommittimeSection: React.FC = () => {
         <div>
           <p className="text-base text-form-text">目標学習時間</p>
           <p className="ml-4 mt-4 text-base font-bold">
-            {committime.targetTime ? Math.floor(committime.targetTime / 60) : 0}
+            {committime?.targetTime
+              ? Math.floor(committime.targetTime / 60)
+              : 0}
             <span className="ml-8 text-base text-form-text">時間</span>
           </p>
         </div>
 
         <p className="text-base text-sm font-semibold">
-          [{displayCommittime.startDate ?? "----"} -{" "}
-          {displayCommittime.endDate ?? "----"}]
+          [{committime?.startDate ?? "----"} - {committime?.endDate ?? "----"}]
+        </p>
+      </div>
+      <div>
+        <p className="text-base text-form-text">現在の合計学習時間</p>
+        <p className="mt-4 text-base font-semibold">
+          {Math.floor(totalStudyTimeByPeriod / 60)}
+          <span className="mx-4 text-base text-form-text font-medium">
+            時間
+          </span>
+          {totalStudyTimeByPeriod % 60}
+          <span className="mx-4 text-base text-form-text font-medium">分</span>
         </p>
       </div>
     </section>

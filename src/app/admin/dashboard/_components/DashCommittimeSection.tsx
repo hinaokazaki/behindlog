fe648b470e04e146d3ac650eef8afa5e0e5059d2
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-03-25 23:01:01 +0800

    feat: add css style

--- src/app/admin/dashboard/_components/DashCommittimeSection.tsx
+++ src/app/admin/dashboard/_components/DashCommittimeSection.tsx
@@ -57,50 +57,57 @@ const DashCommittimeSection: React.FC = () => {
   return (
     <section className="mx-auto mb-4 w-full min-w-[580px] max-w-[760px] rounded-3xl bg-white p-6 shadow-md">
       <BlockTitle title="Commit Time" />
-      <div className="mt-6 flex items-center justify-between gap-8">
-        <div className="h-[180px] w-[180px]">
-          <ResponsiveContainer width="100%" height="100%">
-            <PieChart>
-              <Pie
-                data={chartData}
-                dataKey="value"
-                innerRadius={45}
-                outerRadius={75}
-                paddingAngle={2}
-                stroke="none"
-              >
-                <Cell fill="#F58C7B" />
-                <Cell fill="#E9DB9B" />
-              </Pie>
-            </PieChart>
-          </ResponsiveContainer>
+      <div className="flex gap-8">
+        <div className="flex items-center justify-between gap-8">
+          <div className="h-[180px] w-[180px]">
+            <ResponsiveContainer width="100%" height="100%">
+              <PieChart>
+                <Pie
+                  data={chartData}
+                  dataKey="value"
+                  innerRadius={45}
+                  outerRadius={75}
+                  paddingAngle={2}
+                  stroke="none"
+                >
+                  <Cell fill="#F58C7B" />
+                  <Cell fill="#E9DB9B" />
+                </Pie>
+              </PieChart>
+            </ResponsiveContainer>
+          </div>
         </div>
-      </div>
-      <div className="mt-2 flex items-end justify-between">
         <div>
-          <p className="text-base text-form-text">目標学習時間</p>
-          <p className="ml-4 mt-4 text-base font-bold">
-            {committime?.targetTime
-              ? Math.floor(committime.targetTime / 60)
-              : 0}
-            <span className="ml-8 text-base text-form-text">時間</span>
-          </p>
-        </div>
+          <div className="mt-2 flex items-end justify-between">
+            <div>
+              <p className="text-base text-form-text">目標学習時間</p>
+              <p className="ml-4 mt-4 text-base font-bold">
+                {committime?.targetTime
+                  ? Math.floor(committime.targetTime / 60)
+                  : 0}
+                <span className="ml-8 text-base text-form-text">時間</span>
+              </p>
+            </div>
 
-        <p className="text-base text-sm font-semibold">
-          [{committime?.startDate ?? "----"} - {committime?.endDate ?? "----"}]
-        </p>
-      </div>
-      <div>
-        <p className="text-base text-form-text">現在の合計学習時間</p>
-        <p className="mt-4 text-base font-semibold">
-          {Math.floor(totalStudyTimeByPeriod / 60)}
-          <span className="mx-4 text-base text-form-text font-medium">
-            時間
-          </span>
-          {totalStudyTimeByPeriod % 60}
-          <span className="mx-4 text-base text-form-text font-medium">分</span>
-        </p>
+            <p className="text-base text-sm font-semibold">
+              [{committime?.startDate ?? "----"} -{" "}
+              {committime?.endDate ?? "----"}]
+            </p>
+          </div>
+          <div>
+            <p className="text-base text-form-text">現在の合計学習時間</p>
+            <p className="mt-4 text-base font-semibold">
+              {Math.floor(totalStudyTimeByPeriod / 60)}
+              <span className="mx-4 text-base text-form-text font-medium">
+                時間
+              </span>
+              {totalStudyTimeByPeriod % 60}
+              <span className="mx-4 text-base text-form-text font-medium">
+                分
+              </span>
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );

c567614cf0a9087b01bf16bf8efbaef53e3a1919
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-04-03 22:59:33 +0800

    feat: update friend's dashboard page

--- src/app/admin/users/[id]/dashboard/page.tsx
+++ src/app/admin/users/[id]/dashboard/page.tsx
@@ -1,18 +1,26 @@
 "use client";
 import SectionTitle from "@/app/_components/SectionTitle";
+import DashCalendarSection from "@/app/admin/dashboard/_components/DashCalendarSection";
 import DashCommittimeSection from "@/app/admin/dashboard/_components/DashCommittimeSection";
 import DashGoalSection from "@/app/admin/dashboard/_components/DashGoalSection";
 import DashTodoSection from "@/app/admin/dashboard/_components/DashTodoSection";
-import DashCalendarSection from "@/app/admin/dashboard/_components/DashCalendarSection";
 
 export default function DashboardPage() {
   return (
-    <div>
+    <div className="h-[calc(100vh-120px)] overflow-hidden px-10 py-4">
       <SectionTitle title="Dashboard" />
-      <DashGoalSection />
-      <DashCalendarSection />
-      <DashTodoSection />
-      <DashCommittimeSection />
+
+      <div className="mt-4 grid h-[calc(100%-80px)] grid-cols-[1.8fr_1.2fr] gap-6">
+        <div className="grid h-full min-h-0 grid-rows-[0.8fr_1.6fr] gap-6">
+          <DashGoalSection />
+          <DashCalendarSection />
+        </div>
+
+        <div className="grid h-full min-h-0 grid-rows-[1.5fr_0.55fr] gap-6">
+          <DashTodoSection />
+          <DashCommittimeSection />
+        </div>
+      </div>
     </div>
   );
 }

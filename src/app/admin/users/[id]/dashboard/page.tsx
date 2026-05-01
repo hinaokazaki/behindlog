b8c64fa0ae18534c8fcf9620584a3d2950472be0
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-05-01 12:38:04 +0800

    refactor: comment off friend dashboard page

--- src/app/admin/users/[id]/dashboard/page.tsx
+++ src/app/admin/users/[id]/dashboard/page.tsx
@@ -1,24 +1,25 @@
-"use client";
-import SectionTitle from "@/app/_components/SectionTitle";
-import DashCalendarSection from "@/app/admin/dashboard/_components/DashCalendarSection";
-import DashCommittimeSection from "@/app/admin/dashboard/_components/DashCommittimeSection";
-import DashGoalSection from "@/app/admin/dashboard/_components/DashGoalSection";
-import DashTodoSection from "@/app/admin/dashboard/_components/DashTodoSection";
+// 次のブランチで修正します
+// "use client";
+// import SectionTitle from "@/app/_components/SectionTitle";
+// import DashCalendarSection from "@/app/admin/dashboard/_components/DashCalendarSection";
+// import DashCommittimeSection from "@/app/admin/dashboard/_components/DashCommittimeSection";
+// import DashGoalSection from "@/app/admin/dashboard/_components/DashGoalSection";
+// import DashTodoSection from "@/app/admin/dashboard/_components/DashTodoSection";
 
-export default function FriendsDashboardPage() {
-  return (
-    <div className="h-auto min-h-[calc(100vh-120px)] px-4 py-4 sm:px-6 lg:h-[calc(100vh-120px)] lg:overflow-hidden lg:px-10">
-      <SectionTitle title="Dashboard" />
-      <div className="mt-4 grid gap-6 lg:h-[calc(100%-80px)] lg:grid-cols-[1.8fr_1.2fr]">
-        <div className="grid gap-6 lg:h-full lg:min-h-0 lg:grid-rows-[0.8fr_1.6fr]">
-          <DashGoalSection />
-          <DashCalendarSection />
-        </div>
-        <div className="grid gap-6 lg:h-full lg:min-h-0 lg:grid-rows-[1.5fr_0.55fr]">
-          <DashTodoSection />
-          <DashCommittimeSection />
-        </div>
-      </div>
-    </div>
-  );
-}
+// export default function FriendsDashboardPage() {
+//   return (
+//     <div className="h-auto min-h-[calc(100vh-120px)] px-4 py-4 sm:px-6 lg:h-[calc(100vh-120px)] lg:overflow-hidden lg:px-10">
+//       <SectionTitle title="Dashboard" />
+//       <div className="mt-4 grid gap-6 lg:h-[calc(100%-80px)] lg:grid-cols-[1.8fr_1.2fr]">
+//         <div className="grid gap-6 lg:h-full lg:min-h-0 lg:grid-rows-[0.8fr_1.6fr]">
+//           <DashGoalSection />
+//           <DashCalendarSection />
+//         </div>
+//         <div className="grid gap-6 lg:h-full lg:min-h-0 lg:grid-rows-[1.5fr_0.55fr]">
+//           <DashTodoSection />
+//           <DashCommittimeSection />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }

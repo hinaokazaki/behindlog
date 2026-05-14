61a106354c7defeb94bdf1338a9cc2ca2022d4f2
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-05-14 14:30:20 +0800

    feat: use friendDashboardQuery adn apply data to each dashboard section component

--- src/app/admin/users/[id]/dashboard/page.tsx
+++ src/app/admin/users/[id]/dashboard/page.tsx
@@ -1,5 +1,7 @@
 "use client";
+import Loading from "@/app/_components/Loading";
 import SectionTitle from "@/app/_components/SectionTitle";
+import { useFriendDashboardQuery } from "@/app/admin/_hooks/useFriendDashboardQuery";
 import DashCalendarSection from "@/app/admin/dashboard/_components/DashCalendarSection";
 import DashCommittimeSection from "@/app/admin/dashboard/_components/DashCommittimeSection";
 import DashGoalSection from "@/app/admin/dashboard/_components/DashGoalSection";
@@ -8,18 +10,29 @@ import { useParams } from "next/navigation";
 
 export default function FriendsDashboardPage() {
   const { id } = useParams<{ id: string }>();
+  const friendDashboardQuery = useFriendDashboardQuery({ id });
+
+  if (friendDashboardQuery.isLoading) return <Loading />;
+
+  if (
+    friendDashboardQuery.error ||
+    !friendDashboardQuery.data?.friendDashboard
+  ) {
+    return <p>友達のダッシュボードの取得でエラーが発生しました。</p>;
+  }
+  const friendDashboard = friendDashboardQuery.data.friendDashboard;
 
   return (
     <div className="h-auto min-h-[calc(100vh-120px)] px-4 py-4 sm:px-6 lg:h-[calc(100vh-120px)] lg:overflow-hidden lg:px-10">
       <SectionTitle title="Dashboard" />
       <div className="mt-4 grid gap-6 lg:h-[calc(100%-80px)] lg:grid-cols-[1.8fr_1.2fr]">
         <div className="grid gap-6 lg:h-full lg:min-h-0 lg:grid-rows-[0.8fr_1.6fr]">
-          <DashGoalSection />
+          <DashGoalSection goals={friendDashboard.goals} />
           <DashCalendarSection userId={id} />
         </div>
         <div className="grid gap-6 lg:h-full lg:min-h-0 lg:grid-rows-[1.5fr_0.55fr]">
-          <DashTodoSection />
-          <DashCommittimeSection />
+          <DashTodoSection todos={friendDashboard.todos} />
+          <DashCommittimeSection totalStudyTime={friendDashboard.committime} />
         </div>
       </div>
     </div>

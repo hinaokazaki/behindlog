21b66cfe0f45d050c761c594c5ec760c6d995f84
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-05-14 14:18:11 +0800

    feat: add friendDashboardSchema

---
+++ src/schemas/friendDashboard.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+import { goalsSchema } from "./goal";
+import { todosSchema } from "./todo";
+import { totalStudyTimeSchema } from "./committime";
+
+// 共通のFriendDashboardモデル
+export const friendDashboardSchema = z.object({
+  todos: todosSchema,
+  goals: goalsSchema,
+  committime: totalStudyTimeSchema.nullable(),
+});
+
+// レスポンス用
+export const friendDashboardResponseSchema = z.object({
+  friendDashboard: friendDashboardSchema,
+});
+
+// 型を生成
+export type FriendDashboard = z.infer<typeof friendDashboardSchema>;
+export type FriendDashboardResponse = z.infer<
+  typeof friendDashboardResponseSchema
+>;

eaa1a3b85db37acbd11f92fe7849f42d720f6ca8
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-05-17 23:49:21 +0800

    feat: add navigation button to create Todo on edit page

--- src/app/admin/dashboard/_components/DashTodoSection.tsx
+++ src/app/admin/dashboard/_components/DashTodoSection.tsx
@@ -2,24 +2,38 @@
 import { Todos } from "@/schemas/todo";
 import BlockTitle from "../../_components/BlockTitle";
 import TodoCardBase from "../../edit/_components/TodoCardBase";
+import Button from "@/app/_components/Button";
+import { useRouter } from "next/navigation";
 
 type DashTodoSectionProps = {
   todos: Todos;
 };
 
 const DashTodoSection: React.FC<DashTodoSectionProps> = ({ todos }) => {
+  const router = useRouter();
+
   return (
     <section className="mx-auto mb-4 w-full min-w-[580px] max-w-[760px] rounded-3xl bg-white p-6 shadow-md">
       <BlockTitle title="Todo" />
       <div className="space-y-2">
         {todos.length === 0 ? (
-          <div className="flex flex-col">
-            <p className="text-base text-body font-semibold">
-              * Todoリストが登録されていません
-            </p>
-            <p className="text-base text-body font-semibold">
-              * 編集ページで作成してみましょう!
-            </p>
+          <div className="flex flex-col items-center">
+            <div className="flex flex-col">
+              <p className="text-base text-subtitle font-semibold">
+                Todoリストが登録されていません
+              </p>
+              <p className="text-base text-subtitle font-semibold">
+                編集ページで作成してみましょう!
+              </p>
+            </div>
+            <div className="mt-4">
+              <Button
+                onClick={() => router.push("/admin/edit")}
+                type="button"
+                color="red"
+                children="編集ページでTodoを作成する"
+              />
+            </div>
           </div>
         ) : (
           todos.map((t) => (

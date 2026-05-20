1f62ddadd4be73886c4edc203ba6268501c81836
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-05-21 00:31:18 +0800

    feat: add CommittimeAlert component

--- src/app/admin/records/[date]/_components/CommittimeAlert.tsx
+++ src/app/admin/records/[date]/_components/CommittimeAlert.tsx
@@ -1,7 +1,45 @@
-interface Props {}
+import Image from "next/image";
+import Button from "@/app/_components/Button";
 
-const CommittimeAlert: React.FC<Props> = ({}) => {
-  return <></>;
+type CommittimeAlertProps = {
+  targetTime: number | null;
+  endDate: string | null;
+  onOpenModal: () => void;
+};
+
+const CommittimeAlert = ({
+  targetTime,
+  endDate,
+  onOpenModal,
+}: CommittimeAlertProps) => {
+  const targetHour = targetTime ? Math.floor(targetTime / 60) : 0;
+
+  return (
+    <div className="mt-6 rounded-3xl bg-white px-10 py-8">
+      <div className="flex items-center gap-3">
+        <Image src="/icons/alert.png" alt="alert icon" width={32} height={32} />
+
+        <p className="text-base text-subtitle font-bold">
+          目標期間が終了しています
+        </p>
+      </div>
+
+      <div className="mt-4 text-base text-body font-semibold">
+        <p>
+          あなたの現在の目標（{targetHour}時間）は {endDate ?? "----"}{" "}
+          に終了しました。
+        </p>
+
+        <p>新しい目標を設定してから記録を続けましょう！</p>
+      </div>
+
+      <div className="mt-8 flex justify-center">
+        <Button color="red" className="px-10 py-3" onClick={onOpenModal}>
+          新しい目標時間を設定する
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default CommittimeAlert;

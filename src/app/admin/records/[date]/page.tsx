90a9c9bf6c996217afdac65f2d8efea4e85e8a01
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-05-20 23:56:27 +0800

    feat: apply CommittimeRecordForm component

--- src/app/admin/records/[date]/page.tsx
+++ src/app/admin/records/[date]/page.tsx
@@ -21,6 +21,7 @@ import Textarea from "@/app/_components/Textarea";
 import BlockTitle from "../../_components/BlockTitle";
 import SectionTitle from "@/app/_components/SectionTitle";
 import Input from "@/app/_components/Input";
+import CommittimeRecordForm from "./_components/CommittimeRecordForm";
 
 type RecordForm = {
   memo: string;
@@ -121,6 +122,9 @@ export default function RecordsPage({ params }: { params: { date: string } }) {
     return <p>Todoの取得でエラーが発生しました: {todoQuery.error.message}</p>;
 
   // committime
+  // 開いた記録が過去の物であれば関係ないがリアルタイムの物の場合に記入日がstartDateとendDateの中にない場合（期限が切れている場合）
+  // は期限切れのアラートを出して再設定を促す。
+  // そのための期限切れを判定するロジックを作成
   const record = recordQuery.data?.dailyRecord ?? null;
   const displayCommittime = {
     targetTime:
@@ -198,71 +202,11 @@ export default function RecordsPage({ params }: { params: { date: string } }) {
       </section>
       <section className="mx-auto mb-4 w-full min-w-[580px] max-w-[760px] rounded-3xl bg-white p-6 shadow-md">
         <BlockTitle title="Commit time" />
-        <div className="mt-2 flex items-end justify-between">
-          <div>
-            <p className="text-base text-form-text">目標学習時間</p>
-            <p className="ml-4 mt-4 text-base font-bold">
-              {displayCommittime.targetTime
-                ? Math.floor(displayCommittime.targetTime / 60)
-                : 0}
-              <span className="ml-8 text-base text-form-text">時間</span>
-            </p>
-          </div>
-
-          <p className="text-base text-sm font-semibold">
-            [{displayCommittime.startDate ?? "----"} -{" "}
-            {displayCommittime.endDate ?? "----"}]
-          </p>
-        </div>
-
-        <div className="mt-4 border-t border-gray-200" />
-
-        <div className="mt-4 grid grid-cols-2 gap-8">
-          <div>
-            <p className="text-base text-form-text">今日の学習時間</p>
-
-            <div className="mt-4 flex items-center gap-3">
-              <div className="flex items-center gap-2">
-                <Input
-                  type="number"
-                  placeholder="0"
-                  className="w-20 rounded-md border-2 p-2 text-center"
-                  {...register("studyHours")}
-                />
-                <span className="text-base text-form-text font-medium">
-                  時間
-                </span>
-              </div>
-
-              <div className="flex items-center gap-2">
-                <Input
-                  type="number"
-                  placeholder="0"
-                  inputMode="numeric"
-                  min={0}
-                  max={59}
-                  className="w-20 rounded-md border-2 p-2 text-center"
-                  {...register("studyMinutes")}
-                />
-                <span className="text-base text-form-text font-medium">分</span>
-              </div>
-            </div>
-          </div>
-
-          <div>
-            <p className="text-base text-form-text">合計学習時間</p>
-            <p className="mt-4 text-base font-semibold">
-              {Math.floor(totalStudyTimeByPeriod / 60)}
-              <span className="mx-4 text-base text-form-text font-medium">
-                時間
-              </span>
-              {totalStudyTimeByPeriod % 60}
-              <span className="mx-4 text-base text-form-text font-medium">
-                分
-              </span>
-            </p>
-          </div>
-        </div>
+        <CommittimeRecordForm
+          displayCommittime={displayCommittime}
+          totalStudyTimeByPeriod={totalStudyTimeByPeriod}
+          register={register}
+        />
       </section>
       <section className="mx-auto mb-4 w-full min-w-[580px] max-w-[760px] rounded-3xl bg-white p-6 shadow-md">
         <BlockTitle title="Note" />

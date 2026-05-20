b254235acd72f52ea21d47b4368b59dcec3c9bdd
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-05-21 00:41:31 +0800

    refactor: update font size of modal mode blocktitle

--- src/app/admin/_components/BlockTitle.tsx
+++ src/app/admin/_components/BlockTitle.tsx
@@ -9,7 +9,7 @@ type Props = {
 const BlockTitle: React.FC<Props> = ({ title, mode = "section" }) => {
   const styles = {
     section: "text-primary text-heading-2 font-en",
-    modal: "text-base text-heading-3 font-jp",
+    modal: "text-base text-subtitle-top font-jp",
   };
 
   return <p className={`mb-3 font-bold ${styles[mode]}`}>{title}</p>;

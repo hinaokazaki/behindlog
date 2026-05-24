438f41a0c727c77ecc8ae8358ccb1d1daa02c78d
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-05-24 23:42:15 +0800

    feat: apply ThemeProvider to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,13 @@ import { Noto_Sans_JP, Poppins } from "next/font/google";
 import { ReactNode } from "react";
 import "./globals.css";
 import Header from "./_components/Header";
+import ThemeProvider from "./_providers/ThemeProvider";
 
-// google font を有効化
-const notoSansJP = Noto_Sans_JP({ subsets: ["latin"], variable: "--font-jp" });
+const notoSansJP = Noto_Sans_JP({
+  subsets: ["latin"],
+  variable: "--font-jp",
+});
+
 const poppins = Poppins({
   subsets: ["latin"],
   variable: "--font-en",
@@ -14,15 +18,21 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
   title: "Behindlog",
-  description: " 学習記録型の継続支援アプリ",
+  description: "学習記録型の継続支援アプリ",
 };
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default async function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
     <html lang="ja" className={`${notoSansJP.variable} ${poppins.variable}`}>
       <body id="body" className="m-0 p-0">
-        <Header />
-        <main>{children}</main>
+        <ThemeProvider>
+          <Header />
+          <main>{children}</main>
+        </ThemeProvider>
       </body>
     </html>
   );

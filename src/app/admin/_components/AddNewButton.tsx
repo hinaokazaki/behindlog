6bb58643d134dfce3daa31b7c91f1818bfcd83c8
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-06-02 00:38:07 +0800

    feat: add new hover style

--- src/app/admin/_components/AddNewButton.tsx
+++ src/app/admin/_components/AddNewButton.tsx
@@ -11,9 +11,9 @@ const AddNewButton: React.FC<Props> = ({ handleAdding, label }) => {
     <button
       type="button"
       onClick={handleAdding}
-      className={`mt-2 flex items-center gap-2 rounded-lg p-3`}
+      className={`mt-2 flex items-center gap-2 rounded-lg p-2`}
     >
-      <div className="flex">
+      <div className="flex rounded-xl border-secondary px-4 py-2 hover:border-[2px]">
         <CirclePlus className="mx-2 h-6 w-6 text-secondary" />
         <span className="mr-2 text-base font-bold">新しい{label}</span>
       </div>

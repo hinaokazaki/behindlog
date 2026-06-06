20f3bbcc06771ed3a3704a29e2896ce17b9354e4
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-06-06 23:26:46 +0800

    feat: add Footer component for top page

---
+++ src/app/_components/Footer.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import Logo from "./Logo";
+
+const Footer = () => {
+  return (
+    <footer className="bg-primary px-20 py-16 text-white">
+      <div className="mx-auto max-w-6xl">
+        <div className="flex items-start justify-between">
+          <Logo />
+
+          <nav className="flex flex-col gap-3 text-body font-bold">
+            <Link href="/">Home</Link>
+            <Link href="#about">About</Link>
+            <Link href="#how-to-use">How to use</Link>
+            <Link href="/contact">Contact</Link>
+          </nav>
+        </div>
+
+        <div className="mt-16 border-t border-white pt-8 text-center text-body font-bold">
+          © 2025 Behindlog. All rights reserved.
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

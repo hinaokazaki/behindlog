86c8c2cab05e621e867fad7a9326b0e3d47001dd
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-06-07 23:59:55 +0800

    feat: add top page components

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -8,6 +8,10 @@ import Button from "../_components/Button";
 import Textarea from "../_components/Textarea";
 import { useEffect } from "react";
 import { supabase } from "@/utils/supabase";
+import Footer from "../_components/Footer";
+import AboutSection from "../_components/AboutSection";
+import HowtoUseSection from "../_components/HowtoUseSection";
+import { HeroSection } from "../_components/HeroSection";
 
 export default function InputTestPage() {
   useEffect(() => {
@@ -19,52 +23,13 @@ export default function InputTestPage() {
   }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-white p-8">
-      <div className="w-full max-w-sm space-y-6">
-        <h1 className="text-brand text-center font-en text-2xl font-bold">
-          Login
-        </h1>
-        <div className="bg-accent mx-auto mb-2 h-[2px] w-12" />
-
-        {/* メールアドレス */}
-        <div>
-          <Label name="email" title="メールアドレス" />
-          <Input
-            id="email"
-            type="email"
-            placeholder="メールアドレス"
-            autoComplete="email"
-          />
-        </div>
-
-        {/* パスワード */}
-        <div>
-          <Label name="password" title="パスワード" />
-          <Input
-            id="password"
-            type="password"
-            placeholder="パスワード"
-            autoComplete="current-password"
-          />
-        </div>
-        {/* コメント */}
-        <div>
-          <Label name="message" title="コメント" />
-          <Textarea id="message" placeholder="コメント" />
-        </div>
-
-        {/* ログインボタン（仮） */}
-        <Button
-          color="red"
-          variant="filled"
-          className="w-full"
-          children="ログイン"
-        />
-
-        <p className="cursor-pointer text-center text-base text-form-text underline">
-          アカウントをお持ちでない方はこちら
-        </p>
-      </div>
-    </main>
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-white p-8">
+        <HeroSection mode="lp" />
+        <HowtoUseSection />
+        <AboutSection />
+      </main>
+      <Footer />
+    </>
   );
 }

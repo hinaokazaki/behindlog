1c92c591e3a63722a787c8a5abd6423587e5ae1d
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-06-09 13:17:47 +0800

    refactor: update design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ import HowtoUseSection from "./_components/HowtoUseSection";
 
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] flex-col items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
+    <div className="min-h-screen">
+      <main className="flex flex-col">
         <HeroSection mode="lp" />
         <HowtoUseSection />
         <AboutSection />
-        <Footer />
       </main>
+      <Footer />
     </div>
   );
 }

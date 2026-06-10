54dc8b92205230d499ca1faee4362352970198a1
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-06-10 21:49:28 +0800

    refactor: cleanup test page

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,17 +1,7 @@
 // app/test/page.tsx
-
 "use client";
-
-import Input from "../_components/Input";
-import Label from "../_components/Label";
-import Button from "../_components/Button";
-import Textarea from "../_components/Textarea";
 import { useEffect } from "react";
 import { supabase } from "@/utils/supabase";
-import Footer from "../_components/Footer";
-import AboutSection from "../_components/AboutSection";
-import HowtoUseSection from "../_components/HowtoUseSection";
-import { HeroSection } from "../_components/HeroSection";
 
 export default function InputTestPage() {
   useEffect(() => {
@@ -24,12 +14,7 @@ export default function InputTestPage() {
 
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-white p-8">
-        <HeroSection mode="lp" />
-        <HowtoUseSection />
-        <AboutSection />
-      </main>
-      <Footer />
+      <main className="flex min-h-screen flex-col items-center justify-center bg-white p-8"></main>
     </>
   );
 }

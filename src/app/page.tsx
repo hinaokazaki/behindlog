7583c227383f147bed10a9e2bbfe1f8afe3b592e
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-06-10 15:55:05 +0800

    refactor: update handleGuestLogin function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,46 @@
+"use client";
+import { supabase } from "@/utils/supabase";
 import AboutSection from "./_components/AboutSection";
 import Footer from "./_components/Footer";
 import { HeroSection } from "./_components/HeroSection";
 import HowtoUseSection from "./_components/HowtoUseSection";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleGuestLogin = async () => {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: process.env.NEXT_PUBLIC_GUEST_EMAIL!,
+      password: process.env.NEXT_PUBLIC_GUEST_PASSWORD!,
+    });
+
+    if (error || !data.session) {
+      alert("Guest login failed");
+      return;
+    }
+
+    await fetch("/api/me", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${data.session.access_token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: "Guest",
+        timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+      }),
+    });
+
+    router.replace("/admin/dashboard");
+  };
+
   return (
     <div className="min-h-screen">
       <main className="flex flex-col">
-        <HeroSection mode="lp" />
+        <HeroSection mode="lp" handleGuestLogin={handleGuestLogin} />
         <HowtoUseSection />
-        <AboutSection />
+        <AboutSection handleGuestLogin={handleGuestLogin} />
       </main>
       <Footer />
     </div>

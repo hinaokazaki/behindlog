5c95a3cb2bc9ac9febe2859f4ff3bc820782bf22
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-06-14 19:19:20 +0800

    add responsive layout for Textarea component

--- src/app/_components/Textarea.tsx
+++ src/app/_components/Textarea.tsx
@@ -9,10 +9,12 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
       <textarea
         {...props}
         ref={ref}
-        className={`${className} text-text-base placeholder-text-muted border-border focus:ring-border/30 min-h-[130px] w-full rounded-md border border-base px-4 py-3 text-form-text transition duration-150 ease-in-out focus:outline-none focus:ring`}
+        className={`placeholder-text-muted focus:ring-border/30 min-h-[130px] w-full rounded-xl border border-base px-4 py-3 text-form-text transition duration-150 ease-in-out focus:outline-none focus:ring sm:text-form-text ${className} `}
       />
     );
   },
 );
+
+Textarea.displayName = "Textarea";
 
 export default Textarea;

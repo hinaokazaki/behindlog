028ca419bbd5178e73633fc1a66ff1f6e909f956
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-06-21 00:12:47 +0800

    refactor: update page title

--- src/app/admin/edit/page.tsx
+++ src/app/admin/edit/page.tsx
@@ -7,7 +7,7 @@ import CommitTimeSection from "./_components/CommitTimeSection";
 export default function EditPage() {
   return (
     <div className="px-4 pb-24 sm:px-6 lg:px-10">
-      <SectionTitle title="Edit your record - 編集" />
+      <SectionTitle title="Edit-編集" />
 
       <div className="mt-4 flex flex-col gap-4 sm:gap-6">
         <GoalSection />

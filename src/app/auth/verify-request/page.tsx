b05194b429ec5cf5a0742cd11c909eba62671741
hinaokazaki <115054955+hinaokazaki@users.noreply.github.com>
2026-07-01 22:39:28 +0800

    feat: add responsive design

--- src/app/auth/verify-request/page.tsx
+++ src/app/auth/verify-request/page.tsx
@@ -2,19 +2,26 @@
 
 export default function VerifyRequestPage() {
   return (
-    <div className="m-auto my-[84px] w-[550px] rounded-xl border-[2.5px] border-foreground px-8 py-6">
-      <h1 className="mb-6 text-subtitle-top text-foreground">
-        メールを確認してください
-      </h1>
-      <p className="text-body text-foreground">
-        入力したメールアドレス宛に確認リンクを送信しました。
-      </p>
-      <p className="text-body text-foreground">
-        リンクをクリックするとログインが完了します。
-      </p>
-      <p className="mt-6 text-body text-foreground">
-        見当たらない場合は迷惑メールフォルダもご確認ください。
-      </p>
+    <div className="flex min-h-screen items-center justify-center px-4 py-24 sm:px-6 lg:px-8">
+      <div className="w-full max-w-[550px] rounded-xl border-[2.5px] border-foreground px-5 py-6 sm:px-8">
+        <h1 className="mb-6 text-center text-subtitle-top text-foreground lg:text-left">
+          メールを確認してください
+        </h1>
+
+        <div className="space-y-2">
+          <p className="text-center text-body text-foreground lg:text-left">
+            入力したメールアドレス宛に確認リンクを送信しました。
+          </p>
+
+          <p className="text-center text-body text-foreground lg:text-left">
+            リンクをクリックするとログインが完了します。
+          </p>
+
+          <p className="mt-6 text-center text-body text-foreground lg:text-left">
+            見当たらない場合は迷惑メールフォルダもご確認ください。
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
